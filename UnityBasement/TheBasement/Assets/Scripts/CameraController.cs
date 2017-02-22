e901de2b98b091da8ba6646fa2d7b0e99af00501
Bund187 <gsiliutop@gmail.com>
2017-02-22 12:31:42 +0100

    Añadiendo al start
    Añadiendo diferentes cosas al empezar

---
+++ UnityBasement/TheBasement/Assets/Scripts/CameraController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    
+    public GameObject target;
+    public GameObject ass;
+    public float distance;
+	public bool isTarget;
+
+	private float camOrthographic;
+	private Vector3 camInitPosition;
+	private bool isZoom=false;
+	
+	void Start(){
+		camOrthographic = Camera.main.orthographicSize;
+		camInitPosition = transform.position;
+	}
+	void Update () {
+
+        if (IsTarget)
+            transform.position = target.transform.position- new Vector3 (0,0, distance);
+		if (isZoom) {
+			if (Camera.main.orthographicSize > 1) {
+				ZoomIn("hobo");
+			}
+		} else {
+			if (Camera.main.orthographicSize < camOrthographic) {
+				ZoomOut(target);
+			} 
+		}
+	}
+
+    public void ZoomIn(string theTarget){
+        if (theTarget == "hobo")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, target.transform.position - new Vector3(0, 0, Camera.main.orthographicSize), 0.1f);
+            Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, Camera.main.orthographicSize - 500, Time.deltaTime * 0.7f);
+        }
+        if (theTarget == "ass")
+        {
+            transform.position = Vector3.MoveTowards(transform.position, ass.transform.position - new Vector3(0, 0, Camera.main.orthographicSize), 0.1f);
+            Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, Camera.main.orthographicSize - 500, Time.deltaTime * 0.7f);
+        }
+
+
+    }
+
+	public void ZoomOut(GameObject theTarget)
+    {
+		transform.position = Vector3.MoveTowards(transform.position,camInitPosition + new Vector3 (0, 0, camOrthographic),0.1f);
+		Camera.main.orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, camOrthographic + 500, Time.deltaTime * 0.7f);
+	}
+
+    public bool IsTarget
+    {
+        get
+        {
+            return isTarget;
+        }
+
+        set
+        {
+            isTarget = value;
+        }
+    }
+	public bool IsZoom {
+		get {
+			return this.isZoom;
+		}
+		set {
+			isZoom = value;
+		}
+	}
+}
---
+++ UnityBasement/TheBasement/Assets/Scripts/CharacterMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+
+    void Start()
+    {
+
+    }
+
+    
+    void FixedUpdate()
+    {
+        Move();
+        
+    }
+
+    void Move()
+    {
+        float xMove = Input.GetAxisRaw("Horizontal");
+        if (xMove > 0f)
+        {
+            transform.Translate(new Vector3(xMove * moveSpeed * Time.deltaTime, 0f, 0f));
+        }
+        if (xMove < 0f)
+        {
+            transform.Translate(new Vector3(xMove * moveSpeed * Time.deltaTime, 0f, 0f));
+
+        }
+    }
+}
